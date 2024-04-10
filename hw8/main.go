3bb4c41578e632f58bb44629470f5124e8eebac2
didoogan <tzd0409@gmail.com>
2024-04-10 10:31:35 +0300

    fix pr comments

--- hw8/main.go
+++ hw8/main.go
@@ -60,9 +60,6 @@ func (r *Round) askQuestion(g *Game) {
 }
 
 func (r *Round) userAnswerQuestion(u User, t Task, g *Game) {
-	g.wg.Add(1)
-	defer g.wg.Done()
-
 	answerDuration := rand.Intn(roundDuration)
 	time.Sleep(time.Second * time.Duration(answerDuration))
 
@@ -76,36 +73,17 @@ func (r *Round) userAnswerQuestion(u User, t Task, g *Game) {
 	g.answerCh <- userAnswer
 
 	fmt.Printf("%v answered %v)%v (%v sec)\n", u.name, randAnswer+1, t.answers[randAnswer], answerDuration)
-
 }
 
-func (r *Round) usersAnswerQuestion(g *Game, ctx context.Context) {
-	ctx, cancelFunc := context.WithCancel(ctx)
-	defer cancelFunc()
-
-	g.wg.Add(1)
-	defer g.wg.Done()
-
-	defer close(g.questionCh)
-
-	answerFunc := func(t Task, g *Game) {
+func (r *Round) usersAnswerQuestion(g *Game) {
+	t, ok := <-g.questionCh
+	if ok {
 		for _, u := range g.users {
 
 			answerTimeout := rand.Intn(roundDuration)
 			time.Sleep(time.Duration(answerTimeout))
 
 			go r.userAnswerQuestion(u, t, g)
-		}
-	}
-	for t := range g.questionCh {
-		select {
-		case <-ctx.Done():
-			fmt.Println("Begining of greceful shut down of usersAnswerQuestion")
-			answerFunc(t, g)
-			fmt.Println("usersAnswerQuestion has been grecefully shut downed")
-			cancelFunc()
-		default:
-			answerFunc(t, g)
 		}
 	}
 }
@@ -116,72 +94,54 @@ type Game struct {
 	rounds      []*Round
 	usersPoints map[User]float64
 	mx          sync.Mutex
-	wg          sync.WaitGroup
 	ctx         context.Context
 	questionCh  chan Task
 	answerCh    chan UserAnswer
 }
 
 func (g *Game) updateResult() {
-	ctx, cancelFunx := context.WithCancel(g.ctx)
-	defer cancelFunx()
-
-	select {
-	case <-ctx.Done():
-		fmt.Println("updateResult has been gracefully shut downed")
-		return
-	default:
-		g.wg.Add(1)
-		defer g.wg.Done()
-
-		defer close(g.answerCh)
-
-		for ua := range g.answerCh {
-			u := ua.user
-
-			g.mx.Lock()
-			points, ok := g.usersPoints[ua.user]
-			if ok {
-				penalty := float64(ua.answerDuration) * 0.1
-				g.usersPoints[u] = points + 1 - penalty
-			} else {
-				g.usersPoints[u] = 1.0
-			}
-			g.mx.Unlock()
+	for ua := range g.answerCh {
+		if ua.answer != ua.task.rightAnswer {
+			continue
 		}
+
+		u := ua.user
+
+		g.mx.Lock()
+		points, ok := g.usersPoints[ua.user]
+		penalty := float64(ua.answerDuration) * 0.1
+		bonus := 1 - penalty
+
+		if ok {
+			g.usersPoints[u] = points + bonus
+		} else {
+			g.usersPoints[u] = bonus
+		}
+		g.mx.Unlock()
 	}
 }
 
 func (g *Game) printResult() {
-	ctx, cancelFunc := context.WithCancel(g.ctx)
-	defer cancelFunc()
+	fmt.Printf("\nResult of %v\n", g.name)
 
-	select {
-	case <-ctx.Done():
-		fmt.Println("printResult has been gracefully shut downed")
-		cancelFunc()
-	default:
-		fmt.Printf("\nResult of %v\n", g.name)
+	var userPointsPairs []struct {
+		user   User
+		points float64
+	}
 
-		var userPointsPairs []struct {
+	for user, points := range g.usersPoints {
+		userPointsPairs = append(userPointsPairs, struct {
 			user   User
 			points float64
-		}
+		}{user: user, points: points})
+	}
 
-		for user, points := range g.usersPoints {
-			userPointsPairs = append(userPointsPairs, struct {
-				user   User
-				points float64
-			}{user: user, points: points})
-		}
+	sort.Slice(userPointsPairs, func(i, j int) bool {
+		return userPointsPairs[i].points > userPointsPairs[j].points
+	})
 
-		sort.Slice(userPointsPairs, func(i, j int) bool {
-			return userPointsPairs[i].points > userPointsPairs[j].points
-		})
-
-		for i, upp := range userPointsPairs {
-			fmt.Printf("%v. %v: %.2f\n", i+1, upp.user.name, upp.points)
-		}
+	for i, upp := range userPointsPairs {
+		fmt.Printf("%v. %v: %.2f\n", i+1, upp.user.name, upp.points)
 	}
 }
 
@@ -189,73 +149,68 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	select {
-	case <-ctx.Done():
-		stop()
-	default:
-		tasks := []Task{
-			{
-				question: "What is the primary purpose of the Go context package?",
-				answers: []string{
-					"To manage database connections",
-					"To handle HTTP requests",
-					"To carry deadlines, cancellation signals, and other request-scoped values",
-					"To create goroutines",
-				},
-				rightAnswer: 2,
+	tasks := []Task{
+		{
+			question: "What is the primary purpose of the Go context package?",
+			answers: []string{
+				"To manage database connections",
+				"To handle HTTP requests",
+				"To carry deadlines, cancellation signals, and other request-scoped values",
+				"To create goroutines",
 			},
-			{
-				question: "Which function creates a derived context with a deadline?",
-				answers: []string{
-					"WithTimeout",
-					"WithCancel",
-					"WithValue",
-					"WithDeadline",
-				},
-				rightAnswer: 3,
+			rightAnswer: 2,
+		},
+		{
+			question: "Which function creates a derived context with a deadline?",
+			answers: []string{
+				"WithTimeout",
+				"WithCancel",
+				"WithValue",
+				"WithDeadline",
 			},
-			{
-				question: "What should you do if a function requires a Context parameter but you’re unsure which context to use?",
-				answers: []string{
-					"Pass a nil context",
-					"Use context.TODO()",
-					"Create a custom context",
-					"Ignore the context",
-				},
-				rightAnswer: 1,
+			rightAnswer: 3,
+		},
+		{
+			question: "What should you do if a function requires a Context parameter but you’re unsure which context to use?",
+			answers: []string{
+				"Pass a nil context",
+				"Use context.TODO()",
+				"Create a custom context",
+				"Ignore the context",
 			},
-		}
-
-		userNames := []string{"Jhon", "Linda", "Mike", "Alice", "David", "Emily", "Henry", "Grace", "Oliver", "Sophia", "Daniel", "Isabella", "William", "Ava", "Samuel"}
-		users := make([]User, len(userNames), len(userNames))
-
-		for i, u := range userNames {
-			users[i] = User{u}
-		}
-
-		game := Game{
-			name:        "The worldwide kahoot chempionship",
-			users:       users,
-			rounds:      make([]*Round, len(tasks), len(tasks)),
-			usersPoints: make(map[User]float64),
-			questionCh:  make(chan Task),
-			answerCh:    make(chan UserAnswer),
-			wg:          sync.WaitGroup{},
-			ctx:         ctx,
-		}
-
-		go game.updateResult()
-
-		for i, t := range tasks {
-			round := Round{task: t, userAnswers: make(map[User]int)}
-			game.rounds[i] = &round
-
-			go round.usersAnswerQuestion(&game, ctx)
-			round.askQuestion(&game)
-		}
-
-		// game.wg.Wait()
-		game.printResult()
+			rightAnswer: 1,
+		},
 	}
 
+	userNames := []string{"Jhon", "Linda", "Mike", "Alice", "David", "Emily", "Henry", "Grace", "Oliver", "Sophia", "Daniel", "Isabella", "William", "Ava", "Samuel"}
+	users := make([]User, len(userNames), len(userNames))
+
+	for i, u := range userNames {
+		users[i] = User{u}
+	}
+
+	game := Game{
+		name:        "The worldwide kahoot chempionship",
+		users:       users,
+		rounds:      make([]*Round, len(tasks), len(tasks)),
+		usersPoints: make(map[User]float64),
+		questionCh:  make(chan Task),
+		answerCh:    make(chan UserAnswer),
+		ctx:         ctx,
+	}
+
+	defer close(game.questionCh)
+	defer close(game.answerCh)
+
+	go game.updateResult()
+
+	for i, t := range tasks {
+		round := Round{task: t, userAnswers: make(map[User]int)}
+		game.rounds[i] = &round
+
+		go round.usersAnswerQuestion(&game)
+		round.askQuestion(&game)
+	}
+
+	game.printResult()
 }
