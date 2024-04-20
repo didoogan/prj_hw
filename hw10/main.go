faeea3ec6d97722ccc0c64afe0ef2c936485c272
Tymoshenko Dmytro <tzd0409@gmail.com>
2024-04-20 19:13:35 +0300

    resolve pr comments

--- hw10/main.go
+++ hw10/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"hw10/entities"
 	"hw10/store"
 	"log"
@@ -31,18 +30,22 @@ func main() {
 	srv := &Server{storage: store.NewMemStore()}
 	r := mux.NewRouter()
 
-	r.HandleFunc(fmt.Sprintf("/%v/%v/%v", apiPrefix, apiVersion, "tasks"), srv.listHandler).Methods(http.MethodGet)
-	r.HandleFunc(fmt.Sprintf("/%v/%v/%v", apiPrefix, apiVersion, "tasks"), srv.createHandler).Methods(http.MethodPost)
-	r.HandleFunc(fmt.Sprintf("/%v/%v/%v/{id}", apiPrefix, apiVersion, "tasks"), srv.updateHandler).Methods(http.MethodPut)
-	r.HandleFunc(fmt.Sprintf("/%v/%v/%v/{id}", apiPrefix, apiVersion, "tasks"), srv.deleteHandler).Methods(http.MethodDelete)
+	s := r.PathPrefix("/api/v1/tasks").Subrouter()
+
+	s.HandleFunc("/", srv.listHandler).Methods(http.MethodGet)
+	s.HandleFunc("/", srv.createHandler).Methods(http.MethodPost)
+	s.HandleFunc("/{id}", srv.updateHandler).Methods(http.MethodPut)
+	s.HandleFunc("/{id}", srv.deleteHandler).Methods(http.MethodDelete)
 
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Printf("Server run error: %s", err)
 	}
 }
 
-func writeResponse(w http.ResponseWriter, data any) {
+func writeResponse(w http.ResponseWriter, data any, statusCode int) {
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+
 	err := json.NewEncoder(w).Encode(data)
 	if err != nil {
 		log.Printf("error: %s", err)
@@ -52,7 +55,7 @@ func writeResponse(w http.ResponseWriter, data any) {
 func (s *Server) listHandler(w http.ResponseWriter, r *http.Request) {
 	tasks := s.storage.List()
 
-	writeResponse(w, tasks)
+	writeResponse(w, tasks, http.StatusOK)
 }
 
 func (s *Server) createHandler(w http.ResponseWriter, r *http.Request) {
@@ -66,8 +69,7 @@ func (s *Server) createHandler(w http.ResponseWriter, r *http.Request) {
 
 	s.storage.Create(&task)
 
-	w.WriteHeader(http.StatusCreated)
-	writeResponse(w, task)
+	writeResponse(w, task, http.StatusCreated)
 }
 
 func (s *Server) updateHandler(w http.ResponseWriter, r *http.Request) {
@@ -91,7 +93,8 @@ func (s *Server) updateHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	writeResponse(w, task)
+
+	writeResponse(w, task, http.StatusOK)
 }
 
 func (s *Server) deleteHandler(w http.ResponseWriter, r *http.Request) {
